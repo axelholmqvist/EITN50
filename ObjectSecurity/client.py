b7510cafe89a7a215e7698d6640850d74977aa7c
Fredrik Magnusson <fredrikmagnusson@Fredriks-MacBook-Pro.local>
2020-09-21 13:34:19 +0200

    Updated documentation and changed some variable/function names

--- ObjectSecurity/client.py
+++ ObjectSecurity/client.py
@@ -5,29 +5,35 @@ import pickle
 import random
 import string
 import base64
-
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives import hashes
-
 from Crypto.Cipher import AES
 
+
+""" Client socket values """
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_PORT = 5004
 BUFFER_SIZE = 64
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet, UDP
+def ecdh_handshake():
+    """
+    Function that manages the handshake performed with the ECDH algorithm.
 
-def handshake():
+    Returns the derived key (shared secret)
+    """
     client_private_key = ec.generate_private_key(ec.SECP384R1, default_backend())
     client_public_key = client_private_key.public_key()
     encoded_client_public_key = client_public_key.public_bytes(Encoding.X962, PublicFormat.CompressedPoint)
-    
+
+    #Receives public key from server
     server_public_key, _ = sock.recvfrom(BUFFER_SIZE)
     print(f"Received from server: {server_public_key}")
 
+    #Sends public key to server
     sock.sendto(encoded_client_public_key, (UDP_IP, UDP_PORT))
     time.sleep(1)
 
@@ -45,7 +51,12 @@ def handshake():
 
     return derived_key
 
-def encrypt(message, key):
+def aes_encrypt(message, key):
+    """
+    Function responsible for encrypting the messages using AES.
+
+    Returns the initialization vector and encrypted message.
+    """
     iv = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(16))
     encoded_iv = iv.encode('utf-8')
 
@@ -55,12 +66,16 @@ def encrypt(message, key):
     
     return encoded_iv, encoded_encrypted_message
 
-def start():
+def start_session():
+    """
+    Starts a new session. Each session starts with a handshake. After the handshake is complete,
+    encrypted messages are sent to the server.
+    """
     sock.sendto(b"Hello", (UDP_IP, UDP_PORT))
     print("\nSent to server: b'Hello'")
-    derived_key = handshake()
-    iv, encrypted_message = encrypt("HEJHEJ!", derived_key)
+    derived_key = ecdh_handshake()
+    iv, encrypted_message = aes_encrypt(input("Write message to server: "), derived_key)
     sock.sendto(iv, (UDP_IP, UDP_PORT))
     sock.sendto(encrypted_message, (UDP_IP, UDP_PORT))
 
-start()
+start_session()
--- ObjectSecurity/server.py
+++ ObjectSecurity/server.py
@@ -5,29 +5,33 @@ import pickle
 import random
 import string
 import base64
-
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives import hashes
-
 from Crypto.Cipher import AES
 
-'''
+"""
+Sources:
+
 https://stackoverflow.com/questions/57286946/python-diffie-hellman-exchange-cryptography-library-shared-key-not-the-same
 https://cryptography.io/en/latest/hazmat/primitives/asymmetric/dh/
-'''
+"""
 
+""" Server socket values """
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_PORT = 5004
 BUFFER_SIZE = 64
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet, UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
-''' ------- HANDSHAKE ------- '''
-def handshake(client_ip, client_port):
+def ecdh_handshake(client_ip, client_port):
+    """
+    Function that manages the handshake performed with the ECDH algorithm.
+
+    Returns the derived key (shared secret)
+    """
     print("Initializing handshake...")
     time.sleep(1)
 
@@ -55,8 +59,21 @@ def handshake(client_ip, client_port):
 
     return derived_key
 
-''' ------- START ------- '''
-def start():
+def aes_decrypt(encrypted_message, key, iv):
+    """
+    Function that decrypts the messages received from the client. 
+
+    Returns the decrypted and decoded message.
+    """
+    decryption_suite = AES.new(key, AES.MODE_CFB, iv)
+    decrypted_message = decryption_suite.decrypt(base64.b64decode(encrypted_message))
+    return decrypted_message.decode()
+
+def start_server():
+    """
+    Starts the server and the session. The server listens for messages from the client.
+    The session consist of the handshake and receiving messages. 
+    """
     print("Starting server...")
     time.sleep(1)
     print("Server is up and running!")
@@ -66,16 +83,11 @@ def start():
         client_ip = address[0]
         client_port = address[1]
         print(f"\n[{client_ip}:{client_port}]: {message}")
-        derived_key = handshake(client_ip, client_port)
+        derived_key = ecdh_handshake(client_ip, client_port)
         iv, _ = sock.recvfrom(BUFFER_SIZE)
         encrypted_message, _ = sock.recvfrom(BUFFER_SIZE)
-        decrypted_message = decrypt(encrypted_message, derived_key, iv)
+        decrypted_message = aes_decrypt(encrypted_message, derived_key, iv)
 
         print(f"\n{decrypted_message}")
 
-def decrypt(encrypted_message, key, iv):
-    decryption_suite = AES.new(key, AES.MODE_CFB, iv)
-    decrypted_message = decryption_suite.decrypt(base64.b64decode(encrypted_message))
-    return decrypted_message.decode()
-
-start()
+start_server()
